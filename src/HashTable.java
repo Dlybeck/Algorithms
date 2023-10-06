3e0da0d94d8c8361c1235105c379d2f8a375db30
dlybeck <dlybeck383@gmail.com>
2023-10-06 08:02:20 -0700

    now added HashTable class

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
 
-public class HashTable {
+public class HashTable <K, V>{
+	
+	private ArrayList<K> keys;
+	private ArrayList<ArrayList<V>> values;
+	
+	public HashTable (int initialSize) {
+		this.keys = new ArrayList<K>(initialSize);
+		this.values = new ArrayList<ArrayList<V>>(10);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
