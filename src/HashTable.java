a4241676c3506fcdd27a8064554f4dd525090096
dlybeck383 <dlybeck@pugetsound.edu>
2023-10-13 11:22:23 -0700

    Finished constructors

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,18 +1,40 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class HashTable <K, V>{
 	
 	private ArrayList<K> keys;
 	private ArrayList<ArrayList<V>> values;
+	int initialSize;
 	
-	public HashTable (int initialSize) {
-		this.keys = new ArrayList<K>(initialSize);
-		this.values = new ArrayList<ArrayList<V>>(10);
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
+	
+	public HashTable(int initialSize) {
+		this.keys = new ArrayList<K>(initialSize); //Initialize Keys
+		
+		//Initialize Values
+		this.values = new ArrayList<ArrayList<V>>(initialSize);
+		for (int i = 0; i < initialSize; i++) {
+			this.values.add(new ArrayList<V>());
+		}
+	}
+	
+	public HashTable() {
+		this(11);
+	}
+	
+	void put (K key, V value) {
+		keys.add(key); //insert key
+		
+		//hash+mod key
+		int index = hashAndMod(key);
+		//insert value
+	}
+	
+	private int hashAndMod(K key) {
+		//how do I hash a generic object?
+	}
+	
 
 }
