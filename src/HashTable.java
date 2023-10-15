6ccac7456f98d2c28d6a23ea40fc6469b2352f30
dlybeck383 <dlybeck@pugetsound.edu>
2023-10-15 13:47:22 -0700

    get and put methods added

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,48 +1,89 @@
-import java.util.ArrayList;
-import java.util.Objects;
+@SuppressWarnings("rawtypes")
 
 
 public class HashTable <K, V>{
 	
-	
-	private KVPair[] keys;
-	private ArrayList<ArrayList<V>> values;
-	int initialSize;
+	private KVPair[][] values;
+	private int total = 0;
 	
 	public static void main(String[] args) {
+		HashTable<Integer, String> HT = new HashTable<>();
+		
+		HT.put(2, "Value 2a");
+		HT.put(3, "Value 3");
+		HT.put(2, "Value2b");
+		System.out.println("\n");
+		
+		System.out.println(HT.get(2));
+		System.out.println(HT.get(3));
+		System.out.println("\n");
 	}
 	
 	public HashTable(int initialSize) {
-		this.keys = new KVPair[initialSize]; //Initialize Keys
-		
 		//Initialize Values
-		this.values = new ArrayList<ArrayList<V>>(initialSize);
-		for (int i = 0; i < initialSize; i++) {
-			this.values.add(new ArrayList<V>());
-		}
+		this.values = new KVPair[initialSize][1];
 	}
-	
 	public HashTable() {
 		this(11);
 	}
 	
-	void put (K key, V value) {
-		keys.add(key); //insert key
-		
+	public void put (K key, V value) {
 		//hash+mod key
-		int index = hashAndMod(key);
+		int rowIndex = hashAndMod(key);
 		
-		//insert value
+		//Create KVP to be added
 		KVPair<K, V> newPair = new KVPair<K, V>(key, value);
-		values.add()
 		
+		KVPair[] row = values[rowIndex];
 		
+		//If there is an empty spot take it
+		if(row[0] == null) {
+			row[0] = newPair;
+		}
+		else { //if the spot is full copy previous values into new array and insert new value
+			row = addToArray(row);	
+			
+			row[row.length - 1] = newPair; 
+		}
+		
+		/*Print values in row
+		for(int i = 0; i < row.length; i++){
+			System.out.print(row[i].value + ", ");
+		}*/
+		
+		total ++;
 	}
 	
+	@SuppressWarnings("unchecked")
+	public V get(K key){
+		//get hashed index
+		int rowIndex = hashAndMod(key);
+		KVPair[] row = values[rowIndex];
+		
+		//check every KVP in row for given key
+		for(int i = 0; i < row.length; i++) {
+			if(row[i].key == key) {
+				return (V) row[i].value; //return value at matching key
+			}
+		}
+		
+		//If it doesn't exist return null
+		return null;
+	}
+	
+	
+	private KVPair[] addToArray(KVPair[] KVPs) {
+		KVPair[] newArray = new KVPair[KVPs.length + 1];
+		System.arraycopy(KVPs, 0, newArray, 0, KVPs.length);
+		
+		return newArray;
+
+	}
+ 	
 	private int hashAndMod(K key) {
 		int hash = key.hashCode();
 		hash &= 0x7fffffff;
-		return (hash % keys.size());
+		return (hash % values.length);
 	}
 	
 	private static class KVPair <K, V>{
