ee2b771736a1794a7e8ea548910e141c1d6cd99d
dlybeck <dlybeck383@gmail.com>
2023-10-24 11:14:51 -0700

    Tree now with nodes instead of arraylist

--- Assignment4/src/RedBlackTree.java
+++ Assignment4/src/RedBlackTree.java
@@ -1,32 +1,52 @@
 import java.util.ArrayList;
 
 @SuppressWarnings("rawtypes")
-public class RedBlackTree <K, V> {
+public class RedBlackTree <K extends Comparable<K>, V> {
 	public static void main(String[] args) {
 		RedBlackTree tree = new RedBlackTree();
-		System.out.println(tree.nodes.get(0));
 	}
 	
 	
-	private ArrayList<Node> nodes;
+	private Node root;
 	
 	public RedBlackTree() {
-		this.nodes = new ArrayList<Node>();
+		this.root = null;
 	}
 	
-	private static class Node <K, V>{
+	public void put(k key, V value) {
+		Node root = findAndAdd(root);
+	}
+	
+	
+	
+	
+	private static class Node <K extends Comparable<K>, V>{
 		private K key;
 		private V value;
 		private boolean isRed;
+		private int size;
 		
 		private Node RChild;
 		private Node LChild;
+		private Node parent;
 		
 		public Node(K key, V value){
 			this.key = key;
 			this.value = value;
 			this.isRed = true;
+			this.size = 1;
 			this.RChild = null;
 			this.LChild = null;
+			this.parent = null;		
+		}
+		
+		public Boolean isLeftChild() {
+			return (this == this.parent.LChild);
+		}
+		
+		@SuppressWarnings("unchecked")
+		public void flipColor() {
+			this.isRed = !this.isRed;
+		}
 	}
 }
