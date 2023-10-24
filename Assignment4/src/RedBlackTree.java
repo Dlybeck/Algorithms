5307592f9fadace7afca71fa1eb762037ef082a9
dlybeck <dlybeck383@gmail.com>
2023-10-24 15:32:05 -0700

    Started Recursion

--- Assignment4/src/RedBlackTree.java
+++ Assignment4/src/RedBlackTree.java
@@ -2,8 +2,14 @@ import java.util.ArrayList;
 
 @SuppressWarnings("rawtypes")
 public class RedBlackTree <K extends Comparable<K>, V> {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		RedBlackTree tree = new RedBlackTree();
+		
+		tree.put(1, "a");
+		tree.put(2, "b");
+		tree.put(3, "c");
+		tree.put(4, "d");
 	}
 	
 	
@@ -13,8 +19,60 @@ public class RedBlackTree <K extends Comparable<K>, V> {
 		this.root = null;
 	}
 	
-	public void put(k key, V value) {
-		Node root = findAndAdd(root);
+	@SuppressWarnings("unchecked")
+	public void put(K key, V value) {
+		//if empty 
+		if(root.key == null) {
+			root.key = key;
+			root.value = value;
+		}
+		else {
+			//add new node
+			Node newNode = findAndAdd(root, key, value);
+			//go back up and fix tree
+			
+			System.out.print(newNode.value);
+		}
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	private Node findAndAdd(Node currentNode, K key, V value) {
+		//go left if possible
+		if(key.compareTo((K) currentNode.key) < 0) {
+			if(currentNode.LChild != null) {
+				findAndAdd(currentNode.LChild, key, value);
+			}
+			else {
+				Node newNode = new Node(key,value);
+				//set up new node
+				currentNode.LChild = newNode;
+				newNode.parent = currentNode;
+				return newNode;
+			}
+		}
+		//go right if possible
+		else if(key.compareTo((K) currentNode.key) > 0) {
+			if(currentNode.RChild != null) {
+				findAndAdd(currentNode.RChild, key, value);
+			}
+			else {
+				Node newNode = new Node(key,value);
+				//set up new node
+				currentNode.RChild = newNode;
+				newNode.parent = currentNode;
+				return newNode;
+			}
+		}
+		//replace value
+		else {
+			currentNode.value = value;
+			return currentNode;
+		}
+		
+		//backup
+		return null;
+		
 	}
 	
 	
